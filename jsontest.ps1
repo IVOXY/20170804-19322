45040500f0643a2c112a002c1ca6f3e4075a134a
Chris Crow <ccrow@lab.local>
2017-10-02 11:44:59 -0700

    Added JSON test stub script

---
+++ jsontest.ps1
@@ -0,0 +1,13 @@
+﻿
+
+
+try {
+    $portgroups = get-content -raw -path ".\cdb\cluster1-portgroups.json" |convertfrom-json
+}
+catch {throw "I don't have a valid portgroup database definition"}
+
+foreach ($portgroup in $portgroups.portgroups) {
+    write-host $portgroup.name
+    write-host $portgroup.vlan
+
+}
